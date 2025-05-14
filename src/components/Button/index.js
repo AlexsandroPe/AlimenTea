7fd1130015afc0343b0f180781cf7f84642ce3a4
Joao <joaovictordamascenorodrigues@gmail.com>
2025-05-14 15:06:39 -0300

    Mudando a cor do botão

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -4,7 +4,7 @@ import Styles from "./styles";
 function Button({ title }) {
   return (
     <TouchableOpacity style={Styles.buttonContainer} activeOpacity={0.96}>
-      <Text style={{ color: "#23232323", fontSize: 18, flex: 1 }}>{title}</Text>
+      <Text style={{ color: "#fff", fontSize: 18, flex: 1 }}>{title}</Text>
     </TouchableOpacity>
   );
 }
