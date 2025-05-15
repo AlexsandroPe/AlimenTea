9749e83f740f206dfa47e430da3232ea05ef807e
Joao <joaovictordamascenorodrigues@gmail.com>
2025-05-15 13:22:33 -0300

    tirando o style de app.js

--- App.js
+++ App.js
@@ -4,13 +4,4 @@ import Routes from "./src/routes";
 
 export default function App() {
   return <Routes />;
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+};
