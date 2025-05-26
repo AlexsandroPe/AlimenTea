ed553a36892906a7aeebd4b58970603d31644728
Alexsandro P. Silva <alexpereirasilv4@gmail.com>
2025-05-26 19:55:35 -0300

    inicia desenvolviento adminProfile

--- src/screens/AdminProfile/index.js
+++ src/screens/AdminProfile/index.js
@@ -1,7 +1,13 @@
+import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Styles from "./styles";
 
 function AdminProfile() {
-  return <SafeAreaView></SafeAreaView>;
+  return (
+    <SafeAreaView style={{ flex: 1 }} edges={[]}>
+      <View style={Styles.container}></View>
+    </SafeAreaView>
+  );
 }
 
 export default AdminProfile;
